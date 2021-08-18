2503367d818eb72f0b33b609527f3af89b5814b6
Mario Sancho <marioscp1998@outlook.es>
2021-08-18 21:01:47 +0200

    do layout 4

--- src/components/projects/Swim.js
+++ src/components/projects/Swim.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import img1 from './images/img_1_1.png';
+import img2 from './images/img_1_2.png';
 
 const swim = () => {
     return (
         <div>
             <h3 className='text-center text-md tracking-widest text-yellow-dessert bg-blue-atlantic'>SWIMMING APP</h3>
-            <div className='bg-white bg-opacity-50'>
+            <div className='bg-white bg-opacity-50 flex flex-col items-center'>
                 <div className='bg-yellow-dessert text-center'>
-                    <p>p1</p>
-                    <p>p2</p>
-                    <p>p3</p>
+                    <p>Web app built for swimmers. The app generates custom swimming workouts.</p>
+                    <p>The user fills the form according to his/her preferences. Swimming blocks are generated and swimming drills are selected based on user’s input.</p>
+                    <p>Overall, this project taught me to work with complex logic.</p>
                 </div>
-                <div className='border-2 border-black'>
-                    img1 & img2
+                <div className=' flex justify-start relative w-10/12 rounded-lg overflow-hidden' style={{'marginLeft': '7.5%'}}>
+                    <img src={img1} className=' w-8/12 h-auto border-2 border-black rounded-md'></img>
+                    <img src={img2} className='absolute left-1/4 w-8/12 h-auto border-2 border-black rounded-md'></img>
                 </div>
                 <div className='text-center text-yellow-dessert bg-blue-atlantic'>
-                    bttn1 & bttn2
+                    <button>Visit Website</button>
+                    <button>See Repo</button>
                 </div>
             </div>
         </div>
--- src/components/projects/Template.js
+++ src/components/projects/Template.js
@@ -5,9 +5,9 @@ const Template = () => {
         <div  className='bg-white bg-opacity-50'>
             <h3 className='text-center text-md tracking-widest text-yellow-dessert bg-blue-atlantic'>WEBSITE TEMPLATE</h3>
             <div  className='bg-yellow-dessert text-center'>
-                <p>p1</p>
-                <p>p2</p>
-                <p>p3</p>
+                <p>Responsive website template for musicians.</p>
+                <p>This website can be used as a template for any local music group. It has a consistent layout & includes animations . It also contains useful elements such as a menu or a carousel. </p>
+                <p>This project helped me develop basic programming skills. </p>
             </div>
             <div className='flex flex-col md:flex-row'>
                 <div className='border-2 border-black'>
